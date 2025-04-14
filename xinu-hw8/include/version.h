1f8c9b7c91cdf36e979f72af364766ced7ae31bf
Paige Harrill <pharrill@morbius.mscsnet.mu.edu>
2025-04-14 18:18:57 -0500

    tryin something

--- xinu-hw8/include/version.h
+++ xinu-hw8/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (nezhaobj) #286 (pharrill@morbius.mscsnet.mu.edu) Thu Apr 10 05:48:14 PM CDT 2025"
+#define VERSION "(Embedded Xinu) (nezhaobj) #288 (pharrill@morbius.mscsnet.mu.edu) Mon Apr 14 06:18:26 PM CDT 2025"
